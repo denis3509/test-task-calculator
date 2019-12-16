bfc4e758f0e435d7d0e63f809d8c8c08cc89cb5c
Denis Bogatyev <denis3509@gmail.com>
2019-12-16 15:26:47 +0300

    start

--- routes/index.js
+++ routes/index.js
@@ -2,6 +2,8 @@ const express = require('express');
 const router = express.Router();
 const indexController = require('../controllers/index');
 
-router.get('/:string', indexController.calculateString);
-
+router.get('/calc/:string', indexController.calculateString);
+router.get('/',(req,res,next)=>{
+  res.send({message: 'hello'});
+})
 module.exports = router;
